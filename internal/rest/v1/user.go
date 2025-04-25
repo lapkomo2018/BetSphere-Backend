4d44208921b7ca086ebda21c834059aec89bbcb5
lapkomo2018 <lapkomo2018@gmail.com>
2025-04-25 20:12:05 +0300

    add endpoint /users/:id

--- internal/rest/v1/user.go
+++ internal/rest/v1/user.go
@@ -1,9 +1,14 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) initUsers(group *gin.RouterGroup) {
 	group.GET("/me", h.authMiddleware, h.getMe)
+	group.GET("/:id", h.getUser)
 }
 
 func (h *Handler) getMe(c *gin.Context) {
@@ -20,4 +25,26 @@ func (h *Handler) getMe(c *gin.Context) {
 	}
 
 	c.JSON(200, user)
+}
+
+func (h *Handler) getUser(c *gin.Context) {
+	userID, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid User ID"})
+		return
+	}
+
+	user, err := h.userService.Get(c.Request.Context(), userID)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to get user"})
+		return
+	}
+
+	c.JSON(200, struct {
+		ID       uint64 `json:"id"`
+		Username string `json:"username"`
+	}{
+		ID:       user.ID,
+		Username: user.Username,
+	})
 }
